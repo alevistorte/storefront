eadff4f3c09963ec6add6d314a9873912febbf6f
Alejandro Vistorte <alevistorte@gmail.com>
2022-07-31 23:31:29 -0400

    Doing some querys and showing results in the template

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
-# Create your views here.
 # Request handler
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Alejandro'})
+    # try:
+    #     product = Product.objects.get(pk=1)
+    # except ObjectDoesNotExist:
+    #     pass
+
+    products = Product.objects.filter(unit_price__range=(20, 30))
+
+    return render(request, 'hello.html', {'name': 'Alejandro', 'products': list(products)})
+    # return HttpResponse(product)
